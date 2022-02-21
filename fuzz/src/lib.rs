dc22c3a105c22fa34db89aa151c0231a37918b70
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2022-02-21 16:25:49 +0100

    Handle special case with remainders

--- fuzz/src/lib.rs
+++ fuzz/src/lib.rs
@@ -17,6 +17,25 @@ impl Target {
         let target = Target::arbitrary_take_rest(Unstructured::new(data))?;
         Ok(target)
     }
+
+    fn is_interesting(&self) -> bool {
+        match self {
+            // Dividing by 0 always errors (and we have unit tests for it) so no
+            // need to fuzz for it.
+            Target::Signed(op) | Target::Unsigned(op)
+                if matches!(op, Op::Div(_, Num::ZERO) | Op::Rem(_, Num::ZERO)) =>
+            {
+                false
+            }
+
+            // `I256::MIN % -1` reports an overflow (the same is true for
+            // primitive tytes `i8::MIN % -1`) but does not overflow in the
+            // reference implementation (since the result doesn't overflow).
+            Target::Signed(Op::Rem(Num::IMIN, Num::UMAX)) => false,
+
+            _ => true,
+        }
+    }
 }
 
 #[derive(Arbitrary, Debug)]
@@ -28,13 +47,13 @@ pub enum Op {
     Rem(Num, Num),
 }
 
-#[derive(Arbitrary, Debug)]
+#[derive(Arbitrary, Debug, Eq, PartialEq)]
 pub struct Num(u128, u128);
 
 impl Num {
-    fn is_zero(&self) -> bool {
-        self.0 == 0 && self.1 == 0
-    }
+    const ZERO: Self = Self(0, 0);
+    const IMIN: Self = Self(i128::MIN as _, 0);
+    const UMAX: Self = Self(u128::MAX, u128::MAX);
 }
 
 impl AsU256 for Num {
@@ -55,16 +74,8 @@ macro_rules! ops {
             Op::Add(a, b) => assert_op!(a, b, $as, overflowing_add),
             Op::Sub(a, b) => assert_op!(a, b, $as, overflowing_sub),
             Op::Mul(a, b) => assert_op!(a, b, $as, overflowing_mul),
-            Op::Div(a, b) => {
-                if !b.is_zero() {
-                    assert_op!(a, b, $as, overflowing_div)
-                }
-            }
-            Op::Rem(a, b) => {
-                if !b.is_zero() {
-                    assert_op!(a, b, $as, overflowing_rem)
-                }
-            }
+            Op::Div(a, b) => assert_op!(a, b, $as, overflowing_div),
+            Op::Rem(a, b) => assert_op!(a, b, $as, overflowing_rem),
         }
     }};
 }
@@ -80,9 +91,11 @@ macro_rules! assert_op {
 /// The AFL fuzzing target funtion.
 pub fn fuzz(data: &[u8]) -> Result<(), Box<dyn Error>> {
     let target = Target::load(data)?;
-    match target {
-        Target::Signed(op) => ops!(op, as_i256),
-        Target::Unsigned(op) => ops!(op, as_u256),
+    if target.is_interesting() {
+        match target {
+            Target::Signed(op) => ops!(op, as_i256),
+            Target::Unsigned(op) => ops!(op, as_u256),
+        }
     }
 
     Ok(())
