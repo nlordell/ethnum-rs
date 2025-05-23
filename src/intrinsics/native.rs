d29feb07dcc391201f92e8dde27dfd862297d630
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-05-23 15:25:57 +0200

    fix

--- src/intrinsics/native.rs
+++ src/intrinsics/native.rs
@@ -4,13 +4,11 @@
 mod add;
 mod ctz;
 mod divmod;
-mod divmod_shared;
+mod divmod_impl;
 mod mul;
 mod rot;
 mod shl;
 mod shr;
 mod sub;
 
-pub use self::{
-    add::*, ctz::*, divmod::*, divmod_shared::*, mul::*, rot::*, shl::*, shr::*, sub::*,
-};
+pub use self::{add::*, ctz::*, divmod::*, divmod_impl::*, mul::*, rot::*, shl::*, shr::*, sub::*};
